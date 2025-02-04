913334ef77e27d0e4ca52e7aff80245976c738ce
danis <ahrnadeev@gmail.com>
2025-02-04 08:08:24 +0300

    Update DragonWebSocket with ping and DragonService with cascade delete and filter

--- src/main/java/utils/CustomWebSocketConfigurator.java
+++ src/main/java/utils/CustomWebSocketConfigurator.java
@@ -9,21 +9,21 @@ public class CustomWebSocketConfigurator extends ServerEndpointConfig.Configurat
 
     @Override
     public void modifyHandshake(ServerEndpointConfig config, HandshakeRequest request, HandshakeResponse response) {
-        // Получаем заголовок Origin
+        // получаем заголовок Origin
         List<String> origins = request.getHeaders().get("Origin");
 
         if (origins != null && !origins.isEmpty()) {
             String origin = origins.get(0);
             System.out.println("[WS] Received Origin: " + origin);
-            // Логируем Origin для отладки
+            // логируем Origin для отладки
         } else {
             System.out.println("[WS] Missing Origin header. Allowing connection for debugging purposes.");
         }
 
-        // TODO: Разрешаем все Origin (временно для отладки)
+        // TODO: разрешаем все Origin (временно для отладки)
         response.getHeaders().put("Access-Control-Allow-Origin", List.of("*"));
 
-        // Вы можете добавить пользовательские атрибуты в конфигурацию, если нужно
+        // пользовательские атрибуты
         // config.getUserProperties().put("customAttribute", "customValue");
     }
 }
