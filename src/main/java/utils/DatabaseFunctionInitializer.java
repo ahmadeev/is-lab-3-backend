bbc31b05cc4262c866b1e215f02986bbee3c1a41
danis <ahrnadeev@gmail.com>
2025-02-05 08:15:41 +0300

    Update additional functions (pt. 2)

--- src/main/java/utils/DatabaseFunctionInitializer.java
+++ src/main/java/utils/DatabaseFunctionInitializer.java
@@ -1,10 +1,9 @@
 package utils;
 
-
 import jakarta.annotation.PostConstruct;
 import jakarta.ejb.Startup;
 import jakarta.inject.Inject;
-import jakarta.inject.Singleton;
+import jakarta.ejb.Singleton;
 import services.DragonService;
 
 @Singleton
@@ -14,19 +13,38 @@ public class DatabaseFunctionInitializer {
     @Inject
     private DragonService dragonService;
 
+    private static final String dropFun1 = "DROP FUNCTION IF EXISTS delete_dragons_by_head(FLOAT, DOUBLE PRECISION);";
+    private static final String dropFun2 = "DROP FUNCTION IF EXISTS count_dragons_by_wingspan(BIGINT);";
+    private static final String dropFun3 = "DROP FUNCTION IF EXISTS get_dragons_by_character(TEXT);";
+    private static final String dropFun4 = "DROP FUNCTION IF EXISTS find_dragon_in_deepest_cave();";
+    private static final String dropFun5 = "DROP FUNCTION IF EXISTS kill_dragon(BIGINT);";
+
+    // select delete_dragons_by_head(eyeCount, toothCount);
+    // не каскадно
     private static final String fun1 = """
-            CREATE OR REPLACE FUNCTION delete_dragons_by_head(head_eyes FLOAT, head_tooth DOUBLE PRECISION)
-            RETURNS VOID AS $$
-            BEGIN
-                DELETE FROM dragon
-                WHERE head_id IN (
-                    SELECT id FROM dragon_head
-                    WHERE eyes_count = head_eyes AND tooth_count = head_tooth
-                );
-            END;
-            $$ LANGUAGE plpgsql;
+        CREATE OR REPLACE FUNCTION delete_dragons_by_head(head_eyes double precision, head_tooth double precision)
+        RETURNS integer AS
+        $$
+        DECLARE
+            deleted_count integer;
+        BEGIN
+            -- Выполнение DELETE запроса
+            DELETE FROM dragon
+            WHERE dragon_head_id IN (
+                SELECT id FROM dragon_head
+                WHERE eyes_count = head_eyes AND tooth_count = head_tooth
+            );
+
+            -- Получаем количество затронутых строк
+            GET DIAGNOSTICS deleted_count = ROW_COUNT;
+
+            -- Возвращаем количество удаленных строк
+            RETURN deleted_count;
+        END;
+        $$ LANGUAGE plpgsql;
     """;
 
+    // select count_dragons_by_wingspan(wingspan);
     private static final String fun2 = """
         CREATE OR REPLACE FUNCTION count_dragons_by_wingspan(wingspan_value BIGINT)
         RETURNS INT AS $$
@@ -36,17 +54,32 @@ public class DatabaseFunctionInitializer {
         $$ LANGUAGE plpgsql;
     """;
 
+    // select get_dragons_by_character('CUNNING');
+    // сравнение с помощью CASE, потому что в системе нет enum классов
     private static final String fun3 = """
             CREATE OR REPLACE FUNCTION get_dragons_by_character(character_value TEXT)
             RETURNS SETOF dragon AS $$
             BEGIN
                 RETURN QUERY
                 SELECT * FROM dragon
-                WHERE character < character_value::dragon_character;
+                WHERE
+                    CASE dragon.character
+                        WHEN 'CUNNING' THEN 1
+                        WHEN 'GOOD' THEN 2
+                        WHEN 'CHAOTIC_EVIL' THEN 3
+                    END
+                    <
+                    CASE character_value
+                        WHEN 'CUNNING' THEN 1
+                        WHEN 'GOOD' THEN 2
+                        WHEN 'CHAOTIC_EVIL' THEN 3
+                    END;
             END;
             $$ LANGUAGE plpgsql;
     """;
 
+    // select find_dragon_in_deepest_cave();
+    // ищет по минимальному coordinates.y
     private static final String fun4 = """
             CREATE OR REPLACE FUNCTION find_dragon_in_deepest_cave()
             RETURNS SETOF dragon AS $$
@@ -61,6 +94,8 @@ public class DatabaseFunctionInitializer {
             $$ LANGUAGE plpgsql;
     """;
 
+    // select kill_dragon(1);
+    // назначает убийцу даже в случае не null убийцы у дракона
     private static final String fun5 = """
         CREATE OR REPLACE FUNCTION kill_dragon(dragon_id BIGINT)
         RETURNS VOID AS $$
@@ -73,12 +108,12 @@ public class DatabaseFunctionInitializer {
             -- Если убийца не найден, создаем нового
             IF existing_killer IS NULL THEN
                 INSERT INTO person(name, eye_color, hair_color, location, birthday, height)
-                VALUES ('Unknown Slayer', 'BLACK', NULL, ROW(0, 0, 0)::location, CURRENT_DATE, 180)
+                VALUES ('Unknown Slayer', 'BLACK', NULL, CAST(ROW(0, 0, 0) AS location), CURRENT_DATE, 180)
                 RETURNING id INTO existing_killer;
             END IF;
 
             -- Назначаем убийцу дракону
-            UPDATE dragon SET killer_id = existing_killer WHERE id = dragon_id;
+            UPDATE dragon SET person_id = existing_killer WHERE id = dragon_id;
         END;
         $$ LANGUAGE plpgsql;
     """;
@@ -86,6 +121,12 @@ public class DatabaseFunctionInitializer {
     @PostConstruct
     public void init() {
         System.out.println("=============== Database functions are getting updated ===============");
+
+        dragonService.executeNativeQuery(dropFun1);
+        dragonService.executeNativeQuery(dropFun2);
+        dragonService.executeNativeQuery(dropFun3);
+        dragonService.executeNativeQuery(dropFun4);
+        dragonService.executeNativeQuery(dropFun5);
 
         dragonService.executeNativeQuery(fun1);
         dragonService.executeNativeQuery(fun2);
